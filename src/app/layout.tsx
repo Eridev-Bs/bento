c63eadb53076e3cb3626503c7634841e671ac436
benjamin <benjaminsequeida95@gmail.com>
2025-03-12 21:16:58 +0100

    deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./icont.css";
+import "./icont.svg";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
